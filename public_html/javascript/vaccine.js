1cb45c1042a0a6155010bfe5e45ba1f609278b28
waldenc <waldenc@sheridancollege.ca>
2021-06-05 19:53:08 -0400

    booking page added

--- public_html/javascript/vaccine.js
+++ public_html/javascript/vaccine.js
@@ -1,7 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(document).ready(function(){
+  $("button").click(function(){
+    var x = $("form").serializeArray();
+    $.each(x, function(i, field){
+      $("#results").append(field.name + ":" + field.value + " ");
+    });
+    $("form").trigger("reset");
+  });
+});
