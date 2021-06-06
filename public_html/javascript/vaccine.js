24c930dbf1dba8aa6c0e11c374e0550bc1b1e530
waldenc <waldenc@sheridancollege.ca>
2021-06-05 22:38:35 -0400

    final edition

--- public_html/javascript/vaccine.js
+++ public_html/javascript/vaccine.js
@@ -13,10 +13,10 @@ $(document).ready(function(){
   for (i=0; i < localStorage.length; i++){
       var array = JSON.parse(localStorage.getItem(i));
       var bookingNum = i + 1;
-      $("#results").append("<h3>Booking "+ bookingNum + ":</h3>");
+      $("#results").append("<li>Booking "+ bookingNum + ":");
       $.each(array, function(i, field){
         $("#results").append(field.name + ":" + field.value + " ");   
        });
-       $("#results").append("<br><br>");
+       $("#results").append("</li><br><br>");
   }
 });
