5797a3eaeebb9af2c9d344b3397e3150fe8e35bf
waldenc <waldenc@sheridancollege.ca>
2021-06-05 21:28:50 -0400

    listing page added

--- public_html/javascript/vaccine.js
+++ public_html/javascript/vaccine.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-  $("button").click(function(){
+  $("#submit").click(function(){
     var x = $("form").serializeArray();
     $.each(x, function(i, field){
       $("#results").append(field.name + ":" + field.value + " ");
     });
     $("form").trigger("reset");
+    localStorage.setItem(localStorage.length, JSON.stringify(x));
   });
+  $("#home").click(function(){
+      location.href = "../index.html";
+  });
+  for (i=0; i < localStorage.length; i++){
+      var array = JSON.parse(localStorage.getItem(i));
+      var bookingNum = i + 1;
+      $("#results").append("Booking "+ bookingNum + ":");
+      $.each(array, function(i, field){
+      $("#results").append(field.name + ":" + field.value + " ");
+    });
+  }
 });
